0b5f7c6e7a009b0e321f70e03c3e33a56a5a8e19
manish choudhary <158202537+manish04-git@users.noreply.github.com>
2025-08-05 22:14:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Navigation from "./component/Navigation";
+import Hero from "./component/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  );
+};
+
+export default App;
